0085b07043066c7580eaaf3d2894d3975ee766d6
anshyyy <anshuman9998@gmail.com>
2021-09-22 20:52:54 +0530

    Day 17

--- 100 Days of Python/17/data.py
+++ 100 Days of Python/17/data.py
@@ -1,3 +1,4 @@
+#i have fetched the data from opentrivia.com
 question_data = [
     {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
                                       "question": "The HTML5 standard was published in 2014.", "correct_answer": "True",

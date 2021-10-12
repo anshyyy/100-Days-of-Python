0d6cbde13dd78911d5c0b8dbdfbe737dfe29c95f
anshyyy <anshuman9998@gmail.com>
2021-10-12 18:53:58 +0530

    Day 30 Exception handling

---
+++ 100 Days of Python/30/main.py
@@ -0,0 +1,9 @@
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+
+print(bmi)

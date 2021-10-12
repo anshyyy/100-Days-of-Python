428ee00c80bc5593b5f12e07a711c7c8189fa651
anshyyy <anshuman9998@gmail.com>
2021-10-12 19:05:45 +0530

    DAY 26 LIST AND DICT. COMPREHENSION (exception handled)

--- 100 Days of Python/26/main.py
+++ 100 Days of Python/26/main.py
@@ -1,34 +1,44 @@
+import pandas
+
 student_dict = {
-    "student": ["Angela", "James", "Lily"], 
+    "student": ["Angela", "James", "Lily"],
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
+# Looping through dictionaries:
 for (key, value) in student_dict.items():
-    #Access key and value
+    # Access key and value
     pass
 
-import pandas
 student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
+# Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
+    # Access index and row
+    # Access row.student or row.score
     pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-#{"A": "Alfa", "B": "Bravo"}
+# TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-mydict = {row.letter:row.code for (index,row) in data.iterrows()}
-#print(mydict)
-word = input("Enter a word:").upper()
-my_mist = [mydict[letter] for letter in word]
-print(my_mist)
+mydict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 
+# print(mydict)
+def generate_phonetic():
+    word = input("Enter a word:").upper()
+    try:
+        my_mist = [mydict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please!")
+        generate_phonetic()
+    else:
+        print(my_mist)
+
+
+generate_phonetic()
